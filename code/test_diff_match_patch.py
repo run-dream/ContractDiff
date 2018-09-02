134c7f6f4c47a5ea28b011fe56d17c6714ec1d89
Rundream <rundream@RundreamdeMacBook-Pro.local>
2018-09-02 23:03:01 +0800

    测试了两种对比文本差异的方法

---
+++ code/test_diff_match_patch.py
@@ -0,0 +1,26 @@
+import os
+import re
+import unicodedata
+import diff_match_patch
+import tools
+
+
+def save_file(diff, path):
+    with open(path, 'a') as file:
+        for line in diff:
+            file.write('flag:{} content:\n{}\n'.format(line[0],line[1]))
+    print('Saved to {}'.format(path))
+
+
+
+filepath1 = '../../material/2-打印出纸质版.ocr.txt'
+filepath2 = '../../material/2-电子版对照.txt'
+diffpath = '../../output/diff_of_patch.txt'
+text1 = tools.get_text(filepath1)
+text2 = tools.get_text(filepath2)
+
+diff = diff_match_patch.diff_match_patch().diff_main(text1,text2,True)
+
+print(diff)
+save_file(diff,tools.get_path(diffpath))
+
---
+++ code/tools.py
@@ -0,0 +1,49 @@
+import os
+import re
+import unicodedata
+
+def get_path(relative_path):
+    filepath = os.path.join(__file__, relative_path)
+    filepath = os.path.abspath(filepath)
+    return filepath
+
+def __read_file(relative_path):
+    """读取文件"""
+    filepath = get_path(relative_path)
+    with open(filepath, 'r') as file:
+        s = file.read()
+    return s
+
+def __strQ2B(ustring):
+    """全角转半角"""
+    rstring = ""
+    for uchar in ustring:
+        inside_code=ord(uchar)
+        if inside_code == 12288:                              #全角空格直接转换            
+            inside_code = 32 
+        elif (inside_code >= 65281 and inside_code <= 65374): #全角字符（除空格）根据关系转化
+            inside_code -= 65248
+        rstring += chr(inside_code)
+    return rstring
+
+def __filter_content(content):
+    """预处理文本内容"""
+    # Replace Chinese Marks
+    content = unicodedata.normalize('NFKC', content)
+    content = __strQ2B(content)
+    # Remove redundant space
+    content = ' '.join(re.split(' +|\t', content)).rstrip()
+    content = re.sub('\n+','\n',content)
+    def empty(matched):
+        value = matched.group('space')
+        return re.sub('\s', '', value)
+    content = re.sub('(?P<space>[^\s\n] [\u4e00-\u9fbb])', empty, content)
+    content = re.sub('(?P<space>\s+\n)', empty, content)
+    content = re.sub('(?P<space>\n\s+)', empty, content)
+    return content
+
+
+def get_text(path):
+    text = __read_file(path)
+    text = __filter_content(text)
+    return text
